65daf5b42d0f5df47b1402ec578f20e5ca9614f4
tahworld <118352970+tahworld@users.noreply.github.com>
2026-09-26 07:13:09 -0400

    Fix Home RowScope weight import

--- app/src/main/java/com/vika/quest/ui/home/HomeScreen.kt
+++ app/src/main/java/com/vika/quest/ui/home/HomeScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
